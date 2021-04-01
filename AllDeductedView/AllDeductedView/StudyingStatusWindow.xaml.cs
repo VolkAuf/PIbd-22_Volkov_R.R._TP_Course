f55595713a18fd032162d6c22ac15994a5b13679
RAFMAN <volkov.rafman@mail.ru>
2021-04-02 00:34:48 +0400

    Comm_2

---
+++ AllDeductedView/AllDeductedView/StudyingStatusWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using AllDeductedBusinessLogic.BindingModels;
+using AllDeductedBusinessLogic.BusinessLogics;
+using AllDeductedBusinessLogic.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Unity;
+
+namespace AllDeductedView
+{
+    /// <summary>
+    /// Логика взаимодействия для StudyingStatusWindow.xaml
+    /// </summary>
+    public partial class StudyingStatusWindow : Window
+    {
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        public int StudyingStatusId
+        {
+            set { }
+        }
+        public int StudentId
+        {
+            set { comboBoxStudentId.SelectedValue = value; }
+        }
+
+        public int Course
+        {
+            set
+            {
+                textBoxCourse.Text = value.ToString();
+            }
+        }
+
+        private readonly StudyingStatusLogic logicSS;
+        public StudyingStatusWindow(StudyingStatusLogic logicSS, StudentLogic logicS)
+        {
+            InitializeComponent();
+            /*List<StudentViewModel> list = logicS.Read(null);
+            if (list != null)
+            {
+                comboBoxStudentId.ItemsSource = list;
+                comboBoxStudentId.SelectedItem = null;
+            }*/
+            comboBoxStudyingBase.Items.Clear();
+            foreach (string position in Enum.GetNames(typeof(StudyingBase)))
+            {
+                comboBoxStudyingBase.Items.Add(position);
+            }
+            comboBoxStudyingForm.Items.Clear();
+            foreach (string position in Enum.GetNames(typeof(StudyingForm)))
+            {
+                comboBoxStudyingForm.Items.Add(position);
+            }
+            this.logicSS = logicSS;
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxCourse.Text))
+            {
+                MessageBox.Show("Заполните поле Course", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (comboBoxStudentId.SelectedValue == null)
+            {
+                MessageBox.Show("Выберите Student", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (comboBoxStudyingForm.SelectedValue == null)
+            {
+                MessageBox.Show("Выберите Forms", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (comboBoxStudyingBase.SelectedValue == null)
+            {
+                MessageBox.Show("Выберите Base", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                logicSS.CreateOrUpdate(new StudyingStatusBindingModel
+                {
+                    StudentId = (int)comboBoxStudentId.SelectedValue,
+                    StudyingForm = (StudyingForm)Enum.Parse(typeof(StudyingForm), comboBoxStudyingForm.SelectedValue.ToString()),
+                    StudyingBase = (StudyingBase)Enum.Parse(typeof(StudyingBase), comboBoxStudyingBase.SelectedValue.ToString()),
+                    Course = Convert.ToInt32(textBoxCourse.Text),
+                });
+                MessageBox.Show("Сохранение прошло успешно", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                DialogResult = true;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            Close();
+        }
+    }
+}
--- AllDeductedView/AllDeductedView/StudyingStatusesWindow.xaml.cs
+++ AllDeductedView/AllDeductedView/StudyingStatusesWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AllDeductedBusinessLogic.BusinessLogics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -9,6 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Unity;
 
 namespace AllDeductedView
 {
@@ -17,9 +19,83 @@ namespace AllDeductedView
     /// </summary>
     public partial class StudyingStatusesWindow : Window
     {
-        public StudyingStatusesWindow()
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+        private readonly StudyingStatusLogic logic;
+
+        public StudyingStatusesWindow(StudyingStatusLogic logic)
         {
             InitializeComponent();
+            this.logic = logic;
+        }
+
+        private void StudyingStatusWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+        private void LoadData()
+        {
+            try
+            {
+                var list = logic.Read(null);
+                if (list != null)
+                {
+                    dataGridStudyingStatuses.ItemsSource = list;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var form = Container.Resolve<StudyingStatusWindow>();
+            if (form.ShowDialog() == true)
+            {
+                LoadData();
+            }
+        }
+
+        private void ButtonUpd_Click(object sender, RoutedEventArgs e)
+        {
+            /*if (dataGridStudyingStatuses.SelectedItems.Count == 1)
+            {
+                var form = Container.Resolve<StudyingStatusWindow>();
+                int id = ((StudyingStatusViewModel)dataGridStudyingStatuses.SelectedItems[0]).WorkId;
+                form.Id = id;
+                form.Duration = ((StudyingStatusViewModel)dataGridStudyingStatuses.SelectedItems[0]).Duration;
+                if (form.ShowDialog() == true)
+                {
+                    LoadData();
+                }
+            }*/
+        }
+
+        private void ButtonDel_Click(object sender, RoutedEventArgs e)
+        {
+            /*if (dataGridStudyingStatuses.SelectedItems.Count == 1)
+            {
+                MessageBoxResult result = (MessageBoxResult)MessageBox.Show("Удалить запись", "Вопрос", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    int id = ((StudyingStatusViewModel)dataGridStudyingStatuses.SelectedItems[0]).WorkId;
+                    try
+                    {
+                        logic.Delete(new StudyingStatusBindingModel { WorkId = id });
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    LoadData();
+                }
+            }*/
+        }
+        private void ButtonRef_Click(object sender, RoutedEventArgs e)
+        {
+            LoadData();
         }
     }
 }
